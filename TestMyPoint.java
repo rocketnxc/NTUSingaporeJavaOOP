c6e8906f1c692bcd06b127e1100aa0f835c7bdd5
Cao Xuân Ngọc <rocketnxc@gmail.com>
2015-06-24 00:53:40 +0700

    My Point exercise

---
+++ TestMyPoint.java
@@ -0,0 +1,18 @@
+package NTUSingapore.Exercise3;
+
+/**
+ * Created by Cao on 6/23/2015.
+ */
+public class TestMyPoint {
+    public static void main(String[] args) {
+        MyPoint p1 = new MyPoint(3, 0);
+        MyPoint p2 = new MyPoint(0, 4);
+        System.out.println(p1.distance(p2));
+        System.out.println(p1.distance(5, 6));
+        MyPoint[] points = new MyPoint[10];
+        for (int i=0; i<10; i++) {
+            points[i] = new MyPoint(i,i);
+            System.out.println(points[i]);
+        }
+    }
+}
